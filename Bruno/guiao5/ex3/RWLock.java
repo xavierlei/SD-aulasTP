d728e803919052274da6b2905a916cd05cfa2591
brunomfv <brunomfv@gmail.com>
2015-01-17 16:40:39 +0000

    rwlock added

--- Bruno/guiao5/ex3/RWLock.java
+++ Bruno/guiao5/ex3/RWLock.java
@@ -1,35 +1,76 @@
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.*;
+import java.util.concurrent.locks.*;
 
 class RWLock {
+  private int readers = 0, writers = 0;
+  private int turn = 0, served = 0;
+  private Lock l = new ReentrantLock();
+  private Condition readOnly = l.newCondition();
+  private Condition writeOnly = l.newCondition();
 
-  public readLock() {
-
+  public void readLock() {
+    lock();
+    try {
+      int ticket = turn;
+      turn++;
+      while(ticket > served && writers > 0) {
+        readOnly.await();
+      readers++;
+      served++;
+      }
+    } finally { unlock(); }
   }
 
-  public readUnlock() {
-
+  public void readUnlock() {
+    lock();
+    try {
+      readers--;
+      writeOnly.signal();
+    } finally { unlock(); }
   }
 
-  public writeLock() {
-
+  public void writeLock() {
+    lock();
+    try {
+      int ticket = turn;
+      turn++;
+      while(ticket > served && (readers > 0 || writers > 0)
+        writeOnly.await();
+      writers++;
+      served++;
+      }
+    } finally { unlock(); }
   }
 
-  public writeUnlock() {
-
+  public void writeUnlock() {
+    lock();
+    try {
+      writers--;
+      readOnly.signal();
+      writeOnly.signal();
+    } finally { unlock(); }
   }
+
+  public void lock() { l.lock(); }
+
+  public void unlock() { l.unlock(); }
 }
 
 class Counter {
   private int c = 0;
 
+  RWLock rwl = new RWLock();
+
   public void increment() {
-    this.c++
+    rwl.writeLock();
+    try {
+      this.c++
+    } finally { rwl.writeUnlock(); }
   }
 
-  public int getCounter() {
-    return this.c;
+  public int getserved() {
+    rwl.readLock();
+    try {
+      return this.c;
+    } finally { rwl.readUnlock(); }
   }
 }
